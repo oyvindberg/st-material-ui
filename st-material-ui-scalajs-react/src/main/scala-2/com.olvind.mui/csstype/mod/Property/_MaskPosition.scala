87a4bebead57c5faa6eb649b35aaaa69bdbaad00
Øyvind Raddum Berg <elacin@gmail.com>
2022-12-14 20:41:23 +0100

    remove scala 2

--- st-material-ui-scalajs-react/src/main/scala-2/com.olvind.mui/csstype/mod/Property/_MaskPosition.scala
+++
@@ -1,8 +0,0 @@
-package com.olvind.mui.csstype.mod.Property
-
-import org.scalablytyped.runtime.StObject
-import scala.scalajs.js
-import scala.scalajs.js.`|`
-import scala.scalajs.js.annotation.{JSGlobalScope, JSGlobal, JSImport, JSName, JSBracketAccess}
-
-trait _MaskPosition[TLength] extends StObject
